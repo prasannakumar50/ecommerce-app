f30d51091a7b110d6bbd961ca72fbc19bce3759c
prasannakumar50 <babblu293@gmail.com>
2025-05-21 11:36:11 +0530

    fix: updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@
 const Footer = () =>(
   <footer className="py-2">
      <div className="text-dark text-center fw-bold fs-6">
-        <p>&copy; My Shopping site. All Rights Reserved.</p>
+        <p>&copy; <h1 className="brand-text m-0" style={{ fontSize: "1rem" }}>
+                Clovibe
+              </h1>. All Rights Reserved.</p>
      </div>
   </footer>
 )
